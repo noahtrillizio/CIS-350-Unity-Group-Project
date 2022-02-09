d855fcf2180688b0ad27954db810df45fa91541a
Jacob-Zydorowicz <96926683+Jacob-Zydorowicz@users.noreply.github.com>
2022-02-09 17:26:18 -0600

    Added Crit Spots and made them semi functional
    Added two scripts, MachineMovement &  ObjectClickDetection as well as crit spot models.

---
+++ Mole Mania/Assets/Scripts/MachineMovement.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Jacob Zydorowicz
+ * Project 2 Mole Mania
+ * This script allows the moles and machine spots to be clicked and changed accordingly
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineMovement : MonoBehaviour
+{
+    public int health = 100;
+    public bool canHit;
+    public GameObject lights;
+    public Material lightColor;
+    
+    // initializes variables, and crit spots
+    void Start()
+    {
+        canHit = true;
+        lightColor = lights.GetComponent<Renderer>().material;
+
+        lightColor.SetColor("_EmissionColor", Color.gray);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+ 
+}
+/*Notes:
+ * Make the light bars require multiple hits to go away so one hit doesn't give it away
+ * 
+ */
---
+++ Mole Mania/Assets/Scripts/ObjectClickDetect.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Jacob Zydorowicz
+ * Project 2 Mole Mania
+ * Makes crit spots on machine move and function with game
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClickDetect : MonoBehaviour
+{
+    public MachineMovement machineMovementScript;
+    private void OnMouseDown()
+    {
+        //when the lights are clicked, they turn off
+        if (machineMovementScript.canHit && (gameObject.CompareTag("CircleLights") || gameObject.CompareTag("TriangleLights")))
+        {
+            machineMovementScript.canHit = false;
+            machineMovementScript.lightColor.DisableKeyword("_EMISSION");
+        }
+
+
+    }
+
+   
+}
