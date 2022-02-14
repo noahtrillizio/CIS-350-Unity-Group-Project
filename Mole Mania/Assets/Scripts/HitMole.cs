b67cf431522ec456a4c3aa8999728e1740ee5744
abreuker <abreuker@mail.bradley.edu>
2022-02-14 11:47:39 -0600

    Added Spawn Manager
    it doesn't play nice with the score manager though since the score manager for some reason won't get attached to the mole prefabs. the score manager only plays nice with the test mole.

--- Mole Mania/Assets/Scripts/HitMole.cs
+++ Mole Mania/Assets/Scripts/HitMole.cs
@@ -24,9 +24,15 @@ public class HitMole : MonoBehaviour
             if (moleMoveScript.isUp && hit.collider.CompareTag("Mole"))
             {
 
-                scoreManObj.score++;
-                moleMoveScript.transform.Translate(Vector3.down * moleMoveScript.speed * Time.deltaTime);
-                moleMoveScript.isUp = false;
+                scoreManObj.score++; //as it is now, you cannot attach the score manager to the prefabs, which is what's breaking the spawn manager 
+                                     //it only works on the test mole.
+                                     //if i delete this line and the reference to the score manager, it works but no longer counts score. - anna
+
+                //moleMoveScript.transform.Translate(Vector3.down * moleMoveScript.speed * Time.deltaTime); //<<<commented this out so we can still go back to it if we need to
+                //moleMoveScript.isUp = false;
+
+                //just going to test another method here so i can see if this will make my spawn manager work - anna
+                Destroy(gameObject);
             }
 
 
---
+++ Mole Mania/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Anna Breuker
+ * Project 2
+ * This scripts manages the moles that spawn and where they spawn.
+ */
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject moles;
+
+    //where the mole positions are stored
+    public float[] spawnX;
+    public float[] spawnZ;
+    public float spawnPosY = 17;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRandomPrefabWithCoroutine());
+    }
+
+    IEnumerator SpawnRandomPrefabWithCoroutine()
+    {
+        //add a 3 second delay before first spawning moles
+        yield return new WaitForSeconds(3f);
+
+        while (true)
+        {
+            SpawnMole();
+
+            float randomDelay = Random.Range(1.5f, 3.0f);
+
+            yield return new WaitForSeconds(randomDelay);
+        }
+    }
+
+    void SpawnMole()
+    {
+        //pick mole hole
+        int locationIndex = Random.Range(0, spawnX.Length);
+
+        //generate a random spawn position from mole holes
+        Vector3 spawnPos = new Vector3(spawnX[locationIndex], spawnPosY, spawnZ[locationIndex]);
+
+        //spawn mole
+        Instantiate(moles, spawnPos, moles.transform.rotation);
+    }
+
+}
