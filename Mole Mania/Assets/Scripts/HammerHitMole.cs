43d3ca4a7abf78eaf3594fec0cdf94192b629f8e
DevilbobHellpants <trillizio3@hotmail.com>
2022-02-25 12:41:19 -0600

    Falling works now

--- Mole Mania/Assets/Scripts/HammerHitMole.cs
+++ Mole Mania/Assets/Scripts/HammerHitMole.cs
@@ -6,6 +6,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HammerHitMole : MonoBehaviour
 {
@@ -16,21 +17,54 @@ public class HammerHitMole : MonoBehaviour
     public AudioSource MoleRelatedSFX;
     public AudioClip molesHit;
     public AudioSource BackgroundMusic;
+    public AudioSource Falling;
+    public AudioSource HitGround;
+
+    public TextMesh scoreText;
+    public TextMesh timeText;
 
     private float soundChanger;
+    private float CurrentSounds = 0;
+
+    private float Timer = 160;
 
     void Start()
     {
         scoreManagerScript = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager>();
         spawnManagerScript = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManager>();
+        scoreText.gameObject.SetActive(false);
+        timeText.gameObject.SetActive(false);
     }
 
     void Update()
     {
-        soundChanger = (scoreManagerScript.score) * .01f;
-        if (scoreManagerScript.score >= 3)
+        if (CurrentSounds == 0)
         {
-            BackgroundMusic.pitch = (1f - soundChanger) + .03f;
+            Falling.Play();
+            CurrentSounds++;
+        }
+        if (CurrentSounds == 1)
+        {
+            Timer--;
+        }
+        if (CurrentSounds == 1 && Timer == 0)
+        {
+            HitGround.Play();
+            CurrentSounds++;
+            scoreText.gameObject.SetActive(true);
+            timeText.gameObject.SetActive(true);
+        }
+        if (CurrentSounds == 2 && Input.GetKeyDown(KeyCode.Space))
+        {
+            BackgroundMusic.Play();
+        }
+        if (CurrentSounds == 3)
+        {
+            soundChanger = (scoreManagerScript.score) * .01f;
+            if (scoreManagerScript.score >= 3)
+            {
+                BackgroundMusic.pitch = (1f - soundChanger) + .03f;
+            }
         }
     }
 
