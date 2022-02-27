05095d7e63d37af29504e4d7a5acdead5d96ebf8
abreuker <abreuker@mail.bradley.edu>
2022-02-27 13:44:14 -0600

    text and endings mostly managed
    just need badEndScene and the moles to explode out of the machine with good end.

--- Mole Mania/Assets/Scripts/EndingManager.cs
+++ Mole Mania/Assets/Scripts/EndingManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 /*
  * Anna Breuker
  * Project 2 Mole Mania
@@ -12,6 +14,9 @@ public class EndingManager : MonoBehaviour
     private ScoreManager scoreManagerScript;
     private SpawnManager spawnManagerScript;
     private MachineMovement machineScript;
+    public Text endingText;
+
+    private bool started;
 
 
     // Start is called before the first frame update
@@ -21,26 +26,55 @@ public class EndingManager : MonoBehaviour
         scoreManagerScript = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager>();
         spawnManagerScript = GameObject.FindGameObjectWithTag("SpawnManager").GetComponent<SpawnManager>();
         machineScript = GameObject.FindGameObjectWithTag("Machine").GetComponent<MachineMovement>();
+
+        endingText.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (scoreManagerScript.score >= 50)
+        if (scoreManagerScript.score >= 50 || timer.gameOver)//score gets above a certain amount or time runs out.
         {
             timer.gameOver = true;
-            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(-30, -40, 0);
-            GameObject.FindGameObjectWithTag("Player").transform.rotation = Quaternion.Euler(20, -90, 0);
-            //need text and the ability to press r to restart.
-            //also this just straight up doesn't work.
+            GameObject.FindGameObjectWithTag("Player").transform.position = new Vector3(-30, -40, 0); //this is buggy.
+            GameObject.FindGameObjectWithTag("Player").transform.rotation = Quaternion.Euler(20, -90, 0); //this is also buggy.
+            if (started == true)
+            {
+
+            }
+            else if (started == false)
+            {
+                StartCoroutine(ChangeBadEndText());
+                started = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.R)) // reset
+            {
+                SceneManager.LoadScene("ArcadeSetup");
+            }
         }
         else if (timer.gameOver && machineScript.correctPats == 9)
         {
-            //can't find the good end bool
-        }
-        else if (timer.gameOver)
-        { 
-            //need some kind of game over text like "you ran out of time! try again?"
+            endingText.text = ("You destroyed the machine and freed the moles!\nPress R to restart");
+            if (Input.GetKeyDown(KeyCode.R)) //reset
+            {
+                SceneManager.LoadScene("ArcadeSetup");
+            }
+
         }
     }
+
+
+    
+    IEnumerator ChangeBadEndText()
+    {
+    endingText.text = ("Your score: " + scoreManagerScript.score + "\nPress R to restart");
+
+    yield return new WaitForSeconds(5f);
+
+    endingText.text = ("Moles killed: " + scoreManagerScript.score + "\nPress R to restart");
+
+        yield return new WaitForSeconds(2f);
+    }
+
 }
