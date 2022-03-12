0c174c9d680df124ae35bba8c8f26014c3baca16
Jacob-Zydorowicz <96926683+Jacob-Zydorowicz@users.noreply.github.com>
2022-03-11 23:25:52 -0600

    Environment change with light

---
+++ Mole Mania/Assets/Scripts/LightBlink.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Jacob Zydorowicz, Anna Breuker, Ian Connors
+ * Project 2 Mole Mania
+ * Makes the light soruce in the scene blink with noise
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LightBlink : MonoBehaviour
+{
+
+    public AudioSource blinkSound;
+    Light myLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = gameObject.GetComponent<Light>();
+        StartCoroutine(blink());
+    }
+
+    IEnumerator blink()
+    {
+        while(true)
+        {
+            myLight.intensity = Random.Range(0f, .5f);
+            blinkSound.Play();
+            yield return new WaitForSeconds(Random.Range(.5f, 2f));
+        }
+        
+    }
+}
